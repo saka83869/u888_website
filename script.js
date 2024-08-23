ff28c0ec38d47b0e21a7db586b89118d20bbb97c
saka83869 <saka@u888.com>
2024-08-23 17:02:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ async function checkDomains() {
         try {
             const response = await axios.get(domain);
             console.log(`Kết nối thành công: ${domain}`);
+            location.replace(domain);
+            break;
         } catch (error) {
             console.log(`Không thể kết nối tới: ${domain}`);
         }
