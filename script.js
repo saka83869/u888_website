d519675441032aead36e7c6f4f9a65e0a464dc4b
saka83869 <saka@u888.com>
2024-08-23 16:51:14 +0700

    update scrip
    update scrip

---
+++ script.js
@@ -0,0 +1,18 @@
+const domains = [
+    'https://www.google.com/',
+    'https://www.youtube.com/',
+    'https://mail.google.com'
+];
+
+async function checkDomains() {
+    for (const domain of domains) {
+        try {
+            const response = await axios.get(domain);
+            console.log(`Kết nối thành công: ${domain}`);
+        } catch (error) {
+            console.log(`Không thể kết nối tới: ${domain}`);
+        }
+    }
+}
+
+checkDomains();
